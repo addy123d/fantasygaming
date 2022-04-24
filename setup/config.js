3be20844d40196237e974b2c570ddf6c94265ebf
Aditya <53331534+addy123d@users.noreply.github.com>
2022-04-24 23:04:13 +0530

    Update config.js

--- setup/config.js
+++ setup/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    url : "mongodb+srv://aditya123d:pmd37S2hqnFwvuFb@fantasygaming.ql6ef.mongodb.net/FANTASYGAMING?retryWrites=true&w=majority",
+    url : "",
     secret : "secretkey"
 }
 
-// pmd37S2hqnFwvuFb
